9a03b1054cd5f6691918942ad9afb2f0a6d7b3b9
Jonathan_Levinrad <jjlevinrad@gmail.com>
2023-06-15 11:55:47 +0300

    nightmodev3

--- static/nightmode.js
+++ static/nightmode.js
@@ -1,3 +1,40 @@
-function toggleNightMode() {
-    document.documentElement.classList.toggle('night-mode');
+// Function to set night mode
+function setNightMode() {
+    document.documentElement.classList.add('night-mode');
+    localStorage.setItem('nightMode', 'true');
   }
+  
+  // Function to set day mode
+  function setDayMode() {
+    document.documentElement.classList.remove('night-mode');
+    localStorage.setItem('nightMode', 'false');
+  }
+  
+  // Function to toggle night mode
+  function toggleNightMode() {
+    const isNightMode = document.documentElement.classList.contains('night-mode');
+    
+    if (isNightMode) {
+      setDayMode();
+    } else {
+      setNightMode();
+    }
+  }
+  
+  // Check if night mode preference is stored
+  const isNightModeStored = localStorage.getItem('nightMode');
+  
+  // Apply night mode if preference exists
+  if (isNightModeStored === 'true') {
+    setNightMode();
+  }
+
+  
+
+  
+  
+  
+  
+  
+  
+  
